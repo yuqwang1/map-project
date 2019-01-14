11f1b4020db9ff98d0f6e6633b09c7cb47582965
Yuqian Wang <iamwangyuqian@yahoo.com>
2019-01-14 17:23:03 -0500

    modify TrieST class

--- proj3/src/main/java/TrieST.java
+++ proj3/src/main/java/TrieST.java
@@ -1,50 +1,30 @@
-/******************************************************************************
- *  Compilation:  javac TrieST.java
- *  Execution:    java TrieST < words.txt
- *  Dependencies: StdIn.java
- *  Data files:   https://algs4.cs.princeton.edu/52trie/shellsST.txt
- *
- *  A string symbol table for extended ASCII strings, implemented
- *  using a 256-way trie.
- *
- *  % java TrieST < shellsST.txt
- *  by 4
- *  sea 6
- *  sells 1
- *  she 0
- *  shells 3
- *  shore 7
- *  the 5
- *
- ******************************************************************************/
+import java.util.*;
 
+/******************************************************************************
+ *  Copyright 2002-2018, Robert Sedgewick and Kevin Wayne.
+ *
+ *  This file is part of algs4.jar, which accompanies the textbook
+ *
+ *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ *      http://algs4.cs.princeton.edu
+ *
+ *
+ *  algs4.jar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  algs4.jar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
+ ******************************************************************************/
 /**
- *  The {@code TrieST} class represents an symbol table of key-value
- *  pairs, with string keys and generic values.
- *  It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
- *  <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
- *  It also provides character-based methods for finding the string
- *  in the symbol table that is the <em>longest prefix</em> of a given prefix,
- *  finding all strings in the symbol table that <em>start with</em> a given prefix,
- *  and finding all strings in the symbol table that <em>match</em> a given pattern.
- *  A symbol table implements the <em>associative array</em> abstraction:
- *  when associating a value with a key that is already in the symbol table,
- *  the convention is to replace the old value with the new value.
- *  Unlike {@link java.util.Map}, this class uses the convention that
- *  values cannot be {@code null}—setting the
- *  value associated with a key to {@code null} is equivalent to deleting the key
- *  from the symbol table.
- *  <p>
- *  This implementation uses a 256-way trie.
- *  The <em>put</em>, <em>contains</em>, <em>delete</em>, and
- *  <em>longest prefix</em> operations take time proportional to the length
- *  of the key (in the worst case). Construction takes constant time.
- *  The <em>size</em>, and <em>is-empty</em> operations take constant time.
- *  Construction takes constant time.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/52trie">Section 5.2</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- */
+
 public class TrieST<Value> {
     private static final int R = 256;        // extended ASCII
 
@@ -67,9 +47,10 @@ public class TrieST<Value> {
 
     /**
      * Returns the value associated with the given key.
+     *
      * @param key the key
      * @return the value associated with the given key if the key is in the symbol table
-     *     and {@code null} if the key is not in the symbol table
+     * and {@code null} if the key is not in the symbol table
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public Value get(String key) {
@@ -81,9 +62,10 @@ public class TrieST<Value> {
 
     /**
      * Does this symbol table contain the given key?
+     *
      * @param key the key
      * @return {@code true} if this symbol table contains {@code key} and
-     *     {@code false} otherwise
+     * {@code false} otherwise
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public boolean contains(String key) {
@@ -95,13 +77,14 @@ public class TrieST<Value> {
         if (x == null) return null;
         if (d == key.length()) return x;
         char c = key.charAt(d);
-        return get(x.next[c], key, d+1);
+        return get(x.next[c], key, d + 1);
     }
 
     /**
      * Inserts the key-value pair into the symbol table, overwriting the old value
      * with the new value if the key is already in the symbol table.
      * If the value is {@code null}, this effectively deletes the key from the symbol table.
+     *
      * @param key the key
      * @param val the value
      * @throws IllegalArgumentException if {@code key} is {@code null}
@@ -120,12 +103,13 @@ public class TrieST<Value> {
             return x;
         }
         char c = key.charAt(d);
-        x.next[c] = put(x.next[c], key, val, d+1);
+        x.next[c] = put(x.next[c], key, val, d + 1);
         return x;
     }
 
     /**
      * Returns the number of key-value pairs in this symbol table.
+     *
      * @return the number of key-value pairs in this symbol table
      */
     public int size() {
@@ -134,6 +118,7 @@ public class TrieST<Value> {
 
     /**
      * Is this symbol table empty?
+     *
      * @return {@code true} if this symbol table is empty and {@code false} otherwise
      */
     public boolean isEmpty() {
@@ -144,32 +129,79 @@ public class TrieST<Value> {
      * Returns all keys in the symbol table as an {@code Iterable}.
      * To iterate over all of the keys in the symbol table named {@code st},
      * use the foreach notation: {@code for (Key key : st.keys())}.
+     *
      * @return all keys in the symbol table as an {@code Iterable}
      */
-
+    public Iterable<String> keys() {
+        return keysWithPrefix("");
+    }
 
     /**
      * Returns all of the keys in the set that start with {@code prefix}.
+     *
      * @param prefix the prefix
      * @return all of the keys in the set that start with {@code prefix},
-     *     as an iterable
+     * as an iterable
      */
+    public Iterable<String> keysWithPrefix(String prefix) {
+        Queue<String> results = new LinkedList<>();
+        Node x = get(root, prefix, 0);
+        collect(x, new StringBuilder(prefix), results);
+        return results;
+    }
+
+    private void collect(Node x, StringBuilder prefix, Queue<String> results) {
+        if (x == null) return;
+        if (x.val != null) results.add(prefix.toString());
+        for (char c = 0; c < R; c++) {
+            prefix.append(c);
+            collect(x.next[c], prefix, results);
+            prefix.deleteCharAt(prefix.length() - 1);
+        }
+    }
 
     /**
      * Returns all of the keys in the symbol table that match {@code pattern},
      * where . symbol is treated as a wildcard character.
+     *
      * @param pattern the pattern
      * @return all of the keys in the symbol table that match {@code pattern},
-     *     as an iterable, where . is treated as a wildcard character.
+     * as an iterable, where . is treated as a wildcard character.
      */
+    public Iterable<String> keysThatMatch(String pattern) {
+        Queue<String> results = new LinkedList<>();
+        collect(root, new StringBuilder(), pattern, results);
+        return results;
+    }
 
+    private void collect(Node x, StringBuilder prefix, String pattern, Queue<String> results) {
+        if (x == null) return;
+        int d = prefix.length();
+        if (d == pattern.length() && x.val != null)
+            results.add(prefix.toString());
+        if (d == pattern.length())
+            return;
+        char c = pattern.charAt(d);
+        if (c == '.') {
+            for (char ch = 0; ch < R; ch++) {
+                prefix.append(ch);
+                collect(x.next[ch], prefix, pattern, results);
+                prefix.deleteCharAt(prefix.length() - 1);
+            }
+        } else {
+            prefix.append(c);
+            collect(x.next[c], prefix, pattern, results);
+            prefix.deleteCharAt(prefix.length() - 1);
+        }
+    }
 
     /**
      * Returns the string in the symbol table that is the longest prefix of {@code query},
      * or {@code null}, if no such string.
+     *
      * @param query the query string
      * @return the string in the symbol table that is the longest prefix of {@code query},
-     *     or {@code null} if no such string
+     * or {@code null} if no such string
      * @throws IllegalArgumentException if {@code query} is {@code null}
      */
     public String longestPrefixOf(String query) {
@@ -188,11 +220,12 @@ public class TrieST<Value> {
         if (x.val != null) length = d;
         if (d == query.length()) return length;
         char c = query.charAt(d);
-        return longestPrefixOf(x.next[c], query, d+1, length);
+        return longestPrefixOf(x.next[c], query, d + 1, length);
     }
 
     /**
      * Removes the key from the set if the key is present.
+     *
      * @param key the key
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
@@ -206,10 +239,9 @@ public class TrieST<Value> {
         if (d == key.length()) {
             if (x.val != null) n--;
             x.val = null;
-        }
-        else {
+        } else {
             char c = key.charAt(d);
-            x.next[c] = delete(x.next[c], key, d+1);
+            x.next[c] = delete(x.next[c], key, d + 1);
         }
 
         // remove subtrie rooted at x if it is completely empty
@@ -219,14 +251,4 @@ public class TrieST<Value> {
                 return x;
         return null;
     }
-
-    /**
-     * Unit tests the {@code TrieST} data type.
-     *
-     * @param args the command-line arguments
-     */
-
-
-
-//Copyright © 2000–2017, Robert Sedgewick and Kevin Wayne.
-// Last updated: Fri Oct 20 12:50:46 EDT 2017.
+}
